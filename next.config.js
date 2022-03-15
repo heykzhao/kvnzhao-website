9a737adc411997a93bf95d336e22b8598d145b2a
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-03-15 16:02:46 -0400

    Add re-directs on resume

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/wordle",
+        destination: "https://wordle5678.netlify.app",
+        permanent: true,
+      },
+      {
+        source: "/weather",
+        destination: "https://weather3point0.netlify.app",
+        permanent: true,
+      },
+      {
+        source: "/calc",
+        destination: "https://heykzhao.github.io/top_calculator/",
+        permanent: true,
+      },
+      {
+        source: "/spotify",
+        destination: "https://github.com/heykzhao/spotify-remove-all-albums",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
