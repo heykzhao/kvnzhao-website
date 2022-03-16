c9a2f0443dfff5ad5f5b33f6eff67d046c99752c
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-03-16 19:59:15 -0400

    Add color pallette to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
+    colors: {
+      "dark-mode-1": "#041C32",
+      "dark-mode-2": "#04293A",
+      "dark-mode-3": "#064663",
+      "dark-mode-4": "#ECB365",
+      "light-mode-1": "#1C658C",
+      "light-mode-2": "#398AB9",
+      "light-mode-3": "#D8D2CB",
+      "light-mode-4": "#EEEEEE",
+    },
     extend: {},
   },
   plugins: [],
