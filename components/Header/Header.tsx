bcb42e6723883bf66b6c64c10c29f0f0e5132d2f
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-03-17 14:01:16 -0400

    Header updates per additional components

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,28 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import type { NextPage } from "next";
 import Image from "next/image";
 
+// Components
 import HeaderDarkModeButton from "./HeaderDarkModeButton";
+import HeaderHamburgerMenu from "./HeaderHamburgerMenu";
+import HeaderDesktopMenu from "./HeaderDesktopMenu";
 
+// Icons
 import { MenuAlt3Icon } from "@heroicons/react/solid";
+import { XIcon } from "@heroicons/react/solid";
 
+// Images
 import kvnLogoDark from "../Header/images/kvn_logo_dark.svg";
 import kvnLogoLight from "../Header/images/kvn_logo_light.svg";
 
 interface ChildProps {
-  darkMode: string;
-  setDarkMode: React.Dispatch<React.SetStateAction<string>>;
+  darkMode: boolean;
+  setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export interface MenuLinks {
+  title: string;
+  path: string;
 }
 
 const Header: NextPage<ChildProps> = ({
   darkMode,
   setDarkMode,
 }: ChildProps) => {
+  const [hamburgerOpen, setHamburgerOpen] = useState<boolean>(false);
+
+  const menuItems: MenuLinks[] = [
+    { title: "About", path: "/about" },
+    { title: "Contact", path: "/contact" },
+  ];
+
+  function handleHamburgerClick() {
+    setHamburgerOpen((prevValue) => !prevValue);
+  }
+
   return (
-    <div className={darkMode}>
+    <div className={darkMode ? "dark relative" : "relative"}>
       <div className="relative flex w-screen justify-center bg-light-mode-4 dark:bg-dark-mode-4">
         <div className="absolute inset-y-0 left-0 flex items-center">
-          <HeaderDarkModeButton setDarkMode={setDarkMode} />
+          <HeaderDarkModeButton darkMode={darkMode} setDarkMode={setDarkMode} />
         </div>
         <div className="flex items-center pt-2 pb-2">
           <div className="hidden w-7 dark:block">
@@ -41,21 +63,26 @@ const Header: NextPage<ChildProps> = ({
           </div>
         </div>
         <div>
-          <div className="absolute inset-y-0 right-0 hidden items-center md:flex">
-            <div className="dark:text-dark-mode-1">About</div>
-            <div className="dark:text-dark-mode-1">Contact</div>
+          <div className="absolute inset-y-0 right-0 hidden items-center p-3 md:flex">
+            <HeaderDesktopMenu menuItems={menuItems} />
           </div>
+          {/* Hamburger menu, hidden on md or larger */}
           <div className="absolute inset-y-0 right-0 flex cursor-pointer items-center p-1 md:hidden">
-            <div className="rounded-lg p-1 hover:bg-dark-mode-1/10">
-              <MenuAlt3Icon className="h-7 w-7" />
-            </div>
+            <button
+              type="button"
+              className="rounded-lg p-1 hover:bg-dark-mode-1/10"
+              onClick={handleHamburgerClick}
+            >
+              {hamburgerOpen ? (
+                <XIcon className="h-7 w-7 text-light-mode-1 dark:text-dark-mode-1" />
+              ) : (
+                <MenuAlt3Icon className="h-7 w-7 text-light-mode-1 dark:text-dark-mode-1" />
+              )}
+            </button>
           </div>
         </div>
       </div>
-      <div className="flex w-screen justify-center bg-light-mode-1 dark:bg-dark-mode-4">
-        <div className="dark:text-dark-mode-1">About</div>
-        <div className="dark:text-dark-mode-1">Contact</div>
-      </div>
+      {hamburgerOpen ? <HeaderHamburgerMenu menuItems={menuItems} /> : <></>}
     </div>
   );
 };
