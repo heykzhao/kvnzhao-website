d7c5f49821e75d72335808e7c746aea219e3937e
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-03-16 20:00:19 -0400

    Initial Contact component

---
+++ components/Contact/Contact.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Contact: NextPage = () => {
+  return (
+    <div className="dark">
+      <div className="dark:bg-dark-mode-1">Hero Section</div>
+    </div>
+  );
+};
+
+export default Contact;
