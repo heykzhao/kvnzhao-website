0f18d162738bf2e3fd2b23a1346fe01b17108514
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-03-17 14:01:01 -0400

    Incorporate dark mode as boolean with slider

--- components/Header/HeaderDarkModeButton.tsx
+++ components/Header/HeaderDarkModeButton.tsx
@@ -1,21 +1,44 @@
 import React from "react";
 import type { NextPage } from "next";
-import { LightBulbIcon } from "@heroicons/react/outline";
+import { Switch } from "@headlessui/react";
+import { LightBulbIcon } from "@heroicons/react/solid";
+import { MoonIcon } from "@heroicons/react/solid";
 
 interface ChildProps {
-  setDarkMode: React.Dispatch<React.SetStateAction<string>>;
+  darkMode: boolean;
+  setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const HeaderDarkModeButton: NextPage<ChildProps> = ({
+  darkMode,
   setDarkMode,
 }: ChildProps) => {
   setDarkMode;
 
   return (
-    <div className="dark">
-      <div>
-        <LightBulbIcon className="h-5 w-5" />
-      </div>
+    <div className="p-3">
+      <Switch
+        checked={darkMode}
+        onChange={setDarkMode}
+        className={`${
+          darkMode ? "bg-dark-mode-1" : "bg-light-mode-1"
+        } relative inline-flex h-10 w-16 items-center rounded-full`}
+      >
+        <span className="sr-only">Toggle Dark Mode</span>
+        <span
+          className={`${
+            darkMode
+              ? "translate-x-7 bg-dark-mode-4"
+              : "translate-x-2 bg-light-mode-4"
+          } inline-block h-7 w-7 transform rounded-full transition duration-200 ease-in-out`}
+        >
+          {darkMode ? (
+            <MoonIcon className="h-7 w-7 text-dark-mode-1" />
+          ) : (
+            <LightBulbIcon className="h-7 w-7 text-light-mode-1" />
+          )}
+        </span>
+      </Switch>
     </div>
   );
 };
