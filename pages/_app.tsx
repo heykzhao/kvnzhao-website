9d6fb13a466e086020cb17c95871ed9a4caf4447
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-03-18 17:05:28 -0400

    Allow for global dark mode state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useState, useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [darkMode, setDarkMode] = useState<boolean>(true);
+
+  useEffect(() => {
+    const darkModeCookie = window.localStorage.getItem("dark-mode");
+    if (darkModeCookie === "light-mode") {
+      setDarkMode(false);
+    }
+  }, []);
+
+  return (
+    <Component {...pageProps} darkMode={darkMode} setDarkMode={setDarkMode} />
+  );
 }
 
 export default MyApp;
