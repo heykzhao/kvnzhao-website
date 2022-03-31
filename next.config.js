2621ffed10f36bbc123bd9ffb14ab37cb7f1d3b5
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-03-31 11:50:45 -0400

    Add graphcms domain for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["media.graphcms.com"],
+  },
   async redirects() {
     return [
       {
