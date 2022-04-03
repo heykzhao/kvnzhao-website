9cf5b8e9b7578150ae4de1448997bc879374c96a
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-04-03 11:41:02 -0400

    Add graphassets.com to domain

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["media.graphcms.com"],
+    domains: ["media.graphcms.com", "media.graphassets.com"],
   },
   async redirects() {
     return [
